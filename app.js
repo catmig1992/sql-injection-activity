1c0ee4dc758d17289bdd5bc1a3cd37ea79203ec9
Catherine Miguel <catmig1992@gmail.com>
2023-02-20 09:12:38 -0800

    add .gitignore file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http = require("http"),
   express = require("express"),
   bodyParser = require("body-parser");
 const sqlite3 = require("sqlite3").verbose();
+
 const app = express();
 app.use(express.static("."));
 app.use(bodyParser.urlencoded({ extended: true }));
